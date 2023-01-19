c8fc0184d86824b804dead7c74bd4d6daa62d1ff
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-01-19 18:20:30 +0000

    wip: test getting a random word to guess

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
+import { useState } from 'react';
+import words from './wordList.json';
+
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => words[Math.floor(Math.random() * words.length)]);
+  console.log(wordToGuess);
   return <h1>Hi</h1>;
 }
 
