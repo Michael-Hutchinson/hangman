f7a0a270d8664c05cb77b6b0812645ef107e524a
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-01-20 17:10:25 +0000

    feat: create keyboard mockup

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,7 +1,43 @@
 import React from 'react';
 
 function Keyboard() {
-  return <h1>keyboard</h1>;
+  const keys = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ];
+  return (
+    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fit, minmax(75px, 1fr))', gap: '.5rem' }}>
+      {keys.map((key) => (
+        <button type="button" key={key}>
+          {key}
+        </button>
+      ))}
+    </div>
+  );
 }
 
 export default Keyboard;
