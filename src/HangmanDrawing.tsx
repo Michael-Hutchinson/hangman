fa17a5f36571508975a12cd561ef36d1153d4143
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-01-20 15:59:17 +0000

    feat: start building hangman outline

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
 function HangmanDrawing() {
-  return <h1>hangman drawing</h1>;
+  return (
+    <div style={{ position: 'relative' }}>
+      <div
+        style={{
+          height: '50px',
+          width: '10px',
+          background: 'black',
+          marginLeft: '120px',
+          position: 'absolute',
+          top: '0',
+          right: '0',
+        }}
+      />
+      <div style={{ height: '10px', width: '200px', background: 'black', marginLeft: '120px' }} />
+      <div style={{ height: '400px', width: '10px', background: 'black', marginLeft: '120px' }} />
+      <div style={{ height: '10px', width: '250px', background: 'black' }} />
+    </div>
+  );
 }
 
 export default HangmanDrawing;
