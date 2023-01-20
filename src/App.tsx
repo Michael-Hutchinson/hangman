bb16751b973a4298ed32bcd2c51db070668bc927
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-01-20 18:17:06 +0000

    feat: add useeffect to check for keypresses

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import HangmanDrawing from './HangmanDrawing';
 import HangmanWord from './HangmanWord';
 import Keyboard from './Keyboard';
@@ -8,6 +8,22 @@ function App() {
   const [wordToGuess, setWordToGuess] = useState(() => words[Math.floor(Math.random() * words.length)]);
   const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
   const incorrectLetters = guessedLetters.filter((letter) => !wordToGuess.includes(letter));
+  useEffect(() => {
+    function addGuessedLetter(letter: string) {
+      if (guessedLetters.includes(letter)) return;
+      setGuessedLetters((currentLetters) => [...currentLetters, letter]);
+    }
+    const handler = (e: KeyboardEvent) => {
+      const { key } = e;
+      if (!key.match(/^[a-z]$/)) return;
+      e.preventDefault();
+      addGuessedLetter(key);
+    };
+    document.addEventListener('keypress', handler);
+    return () => {
+      document.removeEventListener('keypress', handler);
+    };
+  }, [guessedLetters]);
   return (
     <section
       style={{
