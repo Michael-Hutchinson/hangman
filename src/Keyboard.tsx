c4aebb8a52a2a421db7ac8bdfb759ccce42a5554
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-01-20 17:18:30 +0000

    feat: style the keyboard component

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from './Keyboard.module.css';
 
 function Keyboard() {
   const keys = [
@@ -32,7 +33,7 @@ function Keyboard() {
   return (
     <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fit, minmax(75px, 1fr))', gap: '.5rem' }}>
       {keys.map((key) => (
-        <button type="button" key={key}>
+        <button type="button" key={key} className={styles.btn}>
           {key}
         </button>
       ))}
